0a413a5a100cf7240b900372ac1490875f9ff956
Patricio Torres <ptorresmanque@gmail.com>
2017-10-02 18:14:39 -0300

    app sonidos
    app de ejemplo de sonidos

---
+++ src/data/data.animales.ts
@@ -0,0 +1,67 @@
+
+export const ANIMALES = [
+      {
+        nombre: "Caballo",
+        imagen: "assets/animales/caballo.png",
+        audio: "assets/sonidos/caballo.mp3",
+        duracion: 4,
+        reproduciendo: false,
+        descrip: "soy un caballo"
+      },
+      {
+        nombre: "Cabra",
+        imagen: "assets/animales/cabra.png",
+        audio: "assets/sonidos/cabra.wav",
+        duracion: 4,
+        reproduciendo: false,
+        descrip: "soy una cabra y estoy loca"
+      },
+      {
+        nombre: "Cerdo",
+        imagen: "assets/animales/cerdo.png",
+        audio: "assets/sonidos/cerdo.wav",
+        duracion: 2,
+        reproduciendo: false,
+        descrip: "soy cerdito y estoy gordito"
+      },
+      {
+        nombre: "Gallo",
+        imagen: "assets/animales/gallo.png",
+        audio: "assets/sonidos/gallo.mp3",
+        duracion: 4,
+        reproduciendo: false,
+        descrip: "soy gallo y nose"
+      },
+      {
+        nombre: "Mono",
+        imagen: "assets/animales/mono.png",
+        audio: "assets/sonidos/mono.mp3",
+        duracion: 8,
+        reproduciendo: false,
+        descrip: "soy mono y como platanos"
+      },
+      {
+        nombre: "Perro",
+        imagen: "assets/animales/perro.png",
+        audio: "assets/sonidos/perro.mp3",
+        duracion: 5,
+        reproduciendo: false,
+        descrip: "soy el doge farmer"
+      },
+      {
+        nombre: "Serpiente",
+        imagen: "assets/animales/serpiente.png",
+        audio: "assets/sonidos/serpiente.mp3",
+        duracion: 2,
+        reproduciendo: false,
+        descrip: "soy el pecado ja ja ja"
+      },
+      {
+        nombre: "Tigre",
+        imagen: "assets/animales/tigre.png",
+        audio: "assets/sonidos/tigre.mp3",
+        duracion: 2,
+        reproduciendo: false,
+        descrip: "hijo de ti...."
+      }
+];
